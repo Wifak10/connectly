380950969f3b3b41d0947849c9dfb0620f3979e8
wifak10 <wifakayekpola123@gmail.com>
2025-02-27 18:31:18 +0100

    Installation des dépendances des icônes et mise en place de la structure du Header

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 const columns = [
   {
     label: 'Ressources',
@@ -35,11 +36,39 @@ const columns = [
   }
 ];
 </script>
-
 <template>
-  <div>
-    Component: AppFooter
-  </div>
-</template>
+  <UFooter>
+    <template #top>
+      <UContainer>
+          <UFooterColumns :colums="columns">
+          <template #right>
+            <form>
+              <UFormField name="email" label="S'inscrire à notre newsletter" size="lg">
+                <UInput type="email" placeholder="Votre adresse email" required>
+                  <template #trauling>
+                    <UButton type="submit" color="orange" size="xs" icon="mail" label="S'inscrire"/>
+                  </template>
+                </UInput>
+              </UFormField>
+            </form>
+          </template>
+        </UFooterColumns>
+      </UContainer>
+    </template>
 
+    <template #left>
+      <p class="text-sm">
+        Copyright &copy; 2021 Connectly; {{ new Date().getFullYear()}}. Tous droits réservés
+      </p>
+    </template>
+
+    <template #right>
+      <UFooterSocialLinks />
+    </template>
+
+    <template #center>
+      <Ubutton to="https://github.com/Wifak10?tab=repositories  " target="_blank" icon="devicon:githubcodespaces" ></Ubutton>
+    </template>
+  </UFooter>
+</template>
 <style scoped></style>
